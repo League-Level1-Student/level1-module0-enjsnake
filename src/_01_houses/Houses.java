5b32d14336e866037011b30d5c260a51c69adba2
root <root@3d63154f8729>
2021-03-09 02:25:55 +0000

    hi

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,19 @@
 package _01_houses;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
 	public void run() {
-		
+		Robot rob=new Robot();
+		rob.setSpeed(100);
+		rob.moveTo(50, 500);
+		rob.setPenWidth(5);
+		rob.setRandomPenColor();
+		rob.penDown();
+		rob.move(100);
+		rob.turn(90);
+		rob.move(25);
+		rob.turn(90);
+		rob.move(100);
 	}
 }
