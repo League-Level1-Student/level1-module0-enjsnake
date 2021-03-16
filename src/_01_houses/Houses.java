bd1661cefafc8606e7cc97392dda349013e28ac7
root <root@73f6e206e482>
2021-03-16 01:28:06 +0000

    hi

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -3,17 +3,52 @@ package _01_houses;
 import org.jointheleague.graphical.robot.Robot;
 
 public class Houses {
+	Robot rob=new Robot();
 	public void run() {
-		Robot rob=new Robot();
+		
 		rob.setSpeed(100);
 		rob.moveTo(50, 500);
 		rob.setPenWidth(5);
-		rob.setRandomPenColor();
 		rob.penDown();
-		rob.move(100);
+		drawHouse("small");
+		drawHouse("medium");
+		drawHouse("large");
+		drawHouse("small");
+			}
+
+	private void drawHouse(String color, String answer) {
+		// TODO Auto-generated method stub
+		int height=0;
+		String paint="";
+		if(answer.equalsIgnoreCase("small")) {
+			height=60;
+		}
+		else if(answer.equalsIgnoreCase("medium")) {
+			height=120;
+		}
+		else {
+			height=250;
+		}
+		if(color.equalsIgnoreCase("red")) {
+			paint="255,0,0";
+		}
+		else if(color.equalsIgnoreCase("blue")) {
+			
+		}
+		rob.setRandomPenColor();
+		rob.move(height);
 		rob.turn(90);
 		rob.move(25);
 		rob.turn(90);
-		rob.move(100);
+		rob.move(height);
+		rob.turn(-90);
+		rob.setPenColor(0,100,0);
+		rob.move(50);
+		rob.turn(-90);
+
 	}
+	
+	
+		
+	
 }
