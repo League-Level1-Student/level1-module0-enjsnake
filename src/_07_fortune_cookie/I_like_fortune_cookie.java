4186e88c88a9104e07edb2802bf924676c098a76
root <root@edf14d5156dc>
2021-04-27 01:27:36 +0000

    Initial Commit

--- src/_07_fortune_cookie/I_like_fortune_cookie.java
+++ src/_07_fortune_cookie/I_like_fortune_cookie.java
@@ -1,7 +1,9 @@
 package _07_fortune_cookie;
 
 public class I_like_fortune_cookie {
-
+	public void showButton() {
+		System.out.println("Button Pressed!");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
