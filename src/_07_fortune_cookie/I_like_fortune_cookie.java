661328ecee3af81fc46e62b07364cc3faea9b1e8
root <root@e00db3143a89>
2021-05-04 01:27:43 +0000

    Initial Commit

--- src/_07_fortune_cookie/I_like_fortune_cookie.java
+++ src/_07_fortune_cookie/I_like_fortune_cookie.java
@@ -1,12 +1,30 @@
 package _07_fortune_cookie;
 
-public class I_like_fortune_cookie {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class I_like_fortune_cookie implements ActionListener {
 	public void showButton() {
+		JFrame frame=new JFrame();
+		frame.setVisible(true);
+	    JButton button = new JButton();
+	    frame.add(button);
+	    frame.pack();
+	    button.addActionListener(this);
 		System.out.println("Button Pressed!");
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Woohoo");
 	}
 
 }
