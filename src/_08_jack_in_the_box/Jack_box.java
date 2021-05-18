ecf5cca21b2cb97a2d1c332a75f2dc0d873ff1e4
root <root@70a0c8b44149>
2021-05-18 01:26:12 +0000

    Initial Commit

---
+++ src/_08_jack_in_the_box/Jack_box.java
@@ -0,0 +1,10 @@
+package _08_jack_in_the_box;
+
+public class Jack_box {
+	private void showPicture(funny-flying-ghost-boo-text-happy-halloween)
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
